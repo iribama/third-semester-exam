5870d6c47ea100face9c8a14d6e83119799500c4
Rejoice Anodo <isaacrejoice011@gmail.com>
2023-03-21 21:36:53 +0100

    first commit

---
+++ ingress-rule/ingress-rule.tf
@@ -0,0 +1,61 @@
+resource "kubernetes_ingress_v1" "my-voting-ingress" {
+  metadata {
+    name      = "azure-vote-front"
+    labels = {
+      name = "azure-vote-front"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "voting.rejoiceanodo.me"
+      http {
+        path {
+          backend {
+            service{
+              name = "azure-vote-front"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "microservice-ingress" {
+  metadata {
+    name      = "sock-shop"
+    namespace = "sock-shop"
+    labels = {
+      name = "front-end"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "sock-shop.rejoiceanodo.me"
+      http {
+        path {
+          backend {
+            service{
+              name = "front-end"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
